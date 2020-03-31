8a8a30f19610ff7fd8c9553c5885ccb85a2793a0
Nemanja Petrovic <petrovic.nemi@hotmail.com>
2020-03-31 16:28:55 +0200

    Binarytree Aufbau

---
+++ JEE_uebungen/src/binoryTree/BinaryTree.java
@@ -0,0 +1,14 @@
+package binoryTree;
+
+public class BinaryTree {
+	int tree [] = {12, 8, 17, 3, 9, 14, 21, 1, 6 };
+	
+	public static void main(String [] args) {
+		BinaryTree bt = new BinaryTree();
+		bt.sortieren();
+		
+	}
+	public void sortieren() {
+		
+	}
+}
