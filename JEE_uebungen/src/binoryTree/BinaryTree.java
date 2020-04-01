fbfcaa1fb4f37ded414c8731e07741b128f92abb
Nemanja Petrovic <petrovic.nemi@hotmail.com>
2020-04-01 10:46:04 +0200

    Binarytree fertig

--- JEE_uebungen/src/binoryTree/BinaryTree.java
+++ JEE_uebungen/src/binoryTree/BinaryTree.java
@@ -1,14 +1,55 @@
 package binoryTree;
 
 public class BinaryTree {
-	int tree [] = {12, 8, 17, 3, 9, 14, 21, 1, 6 };
-	
-	public static void main(String [] args) {
-		BinaryTree bt = new BinaryTree();
-		bt.sortieren();
-		
+	int[] binarytree = new int[256];
+	private int maxTiefe = 0;
+
+	public void add(int value) {
+		this.add(value, 0, 1);
 	}
-	public void sortieren() {
-		
+
+	public void add(int value, int pos, int tiefe) {
+		if (pos < this.binarytree.length) {
+			if (binarytree[pos] == 0) {
+				// Tiefe bekannt
+				if (tiefe > maxTiefe) {
+					maxTiefe = tiefe;
+				}
+				binarytree[pos] = value;
+			} else {
+				if (binarytree[pos] > value) {
+					// linke Seite
+					add(value, 2 * pos + 1, tiefe + 1);
+				} else {
+					// rechte Seite
+					add(value, 2 * pos + 2, tiefe + 1);
+				}
+			}
+		}
+	}
+
+	public void print() {
+		System.out.println("Tiefe: " + maxTiefe);
+		System.out.print("[");
+		for (int i = 0; i < this.binarytree.length; i++) {
+			System.out.print(this.binarytree[i]);
+			if (i < this.binarytree.length - 1) {
+				System.out.print(", ");
+			}
+		}
+		System.out.print("]");
+	}
+
+	public static void main(String[] args) {
+		BinaryTree bt = new BinaryTree();
+		bt.add(12);
+		bt.add(8);
+		bt.add(14);
+		bt.add(22);
+		bt.add(1);
+		bt.add(87);
+		bt.add(69);
+		bt.add(92);
+		bt.print();
 	}
 }
